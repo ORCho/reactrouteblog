8ba7d42637cb7d3d237be05bc5f8f94fdec8d182
CH <dancimonkey@gmail.com>
2020-09-25 00:06:17 +0800

    good

--- src/component/portfolio/portname.js
+++ src/component/portfolio/portname.js
@@ -3,7 +3,7 @@ const portname = [
         id: 1,
         name: "Data Visulization with D3",
         projectURL: "https://datavizbych.herokuapp.com/",
-        imageURL:"https://static.vecteezy.com/system/resources/thumbnails/000/246/237/original/love-and-peace-hand-drawn-doodle-and-lettering.jpg",
+        imageURL:"./project1.png",
         details:"This is a D3 project.",
 },
 {
