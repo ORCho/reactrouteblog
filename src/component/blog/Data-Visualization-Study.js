164de4f513e598d73f9c33ca661dacac8a0c81b4
CH <dancimonkey@gmail.com>
2021-03-19 12:43:16 +0800

    update

--- src/component/blog/Data-Visualization-Study.js
+++ src/component/blog/Data-Visualization-Study.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function blog() {
     return (
-        <div className="blog" >Coding is fun</div>
+        <div className="blog" >These days I happened to find a course which could teach me how to use codes to demonstrate business data. I am used to play with excel in company to explore and demonstrate data. But it is not so efficient for a big amount of raw data. So I learned other software like Tableau and QlikView. But these tools hide too much backend information for me to understand the working logic. So I tried to learn Python and R which has many useful data analysis libraries. These tools are quite useful for me.</div>
     )
 }
 
--- src/component/blog/blog2.js
+++ src/component/blog/blog2.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function blog() {
     return (
-        <div className="blog">To be added</div>
+        <div className="blog">This website is build with React which is a very first personal web site that I have build up.
+        The biggest challenge is to setup the router.
+        I still find it difficult for me to under stand the logic of the router system. But it is fun to build up the website from scratch.</div>
     )
 }
 
