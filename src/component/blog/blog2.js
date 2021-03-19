3748243255db04c6b7cef557b597ac892fcbc16d
CH <dancimonkey@gmail.com>
2021-03-19 12:52:31 +0800

    update

--- src/component/blog/blog2.js
+++ src/component/blog/blog2.js
@@ -4,7 +4,7 @@ function blog() {
     return (
         <div className="blog">This website is build with React which is a very first personal web site that I have build up.
         The biggest challenge is to setup the router.
-        I still find it difficult for me to under stand the logic of the router system. But it is fun to build up the website from scratch.</div>
+        I still find it difficult for me to understand the logic of the router system. But it is fun to build up the website from scratch.</div>
     )
 }
 
